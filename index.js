39ef80da5c3b181be51835674458177961019424
nurmohammad83 <pesfootball83@gmail.com>
2022-11-09 00:04:00 +0600

    Get All Review

--- index.js
+++ index.js
@@ -62,7 +62,6 @@ function run (){
             const review = await cursor.toArray()
             res.send(review)
         })
-
         app.delete('/review/:id', async(req,res)=>{
             const id = req.params.id;
             const query= {_id:ObjectId(id)}
