0fbd69e35ed6d3170476b2f932537dcb1817df18
nurmohammad83 <pesfootball83@gmail.com>
2022-11-08 09:15:56 +0600

    Add env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 const port = process.env.Port || 5000;
@@ -7,6 +8,31 @@ require('dotenv').config()
 // Midelware
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tcnszhx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function run (){
+    try {
+        const servicesCollection = client.db('lawServices').collection('services')
+        app.get('/services', async(req,res)=>{
+            const query = {}
+            const cursor = servicesCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+        app.get('/servicesall', async(req,res)=>{
+            const query = {}
+            const cursor = servicesCollection.find(query)
+            const services = await cursor.limit(3).toArray()
+            res.send(services)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+run()
 
 
 
